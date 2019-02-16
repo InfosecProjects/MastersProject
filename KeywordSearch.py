7465740677b2f133f43653f9f6f85292d96071ae
MastersProject <47504366+MastersProject@users.noreply.github.com>
2019-02-16 00:06:50 +0000

    Update KeywordSearch.py

--- KeywordSearch.py
+++ KeywordSearch.py
@@ -3,30 +3,22 @@ import os
 
 # Open a list of keywords
 f = open("keywords.txt", "r").read().split("\n")
+print("Keywords: ")
 print(f)
 
 # Get a list of all files in a directory (doc or docx for this example)
 cwd = os.getcwd()
 
 for root, dirs, files in os.walk(cwd + "/Documents"):
-   for name in files:
-      print(os.path.join(root, name))
-   for name in dirs:
-      print(os.path.join(root, name))
+    for name in files:
 
-
-files = os.walk(cwd)
-print(files)
-my_file = os.getcwd() + "/Documents/paper.docx"
-
-# Open each .docx file
-document = Document(my_file)
-# For each paragraph in the documents
-for para in document.paragraphs:
-    for keyword in f:
-        if (para.text.find(keyword)) != -1:
-            print(para.text)
+        # Open each .docx file
+        document = Document(os.path.join(root, name))
+        # For each paragraph in the documents
+        for para in document.paragraphs:
+            for keyword in f:
+                if (para.text.find(keyword)) != -1:
+                    print("Found keyword: '" + para.text + "'")
+                    print("File: " + os.path.join(root, name) + "\n")
 
 # Search returns true if found
-
-#print((document,'your search string')())
