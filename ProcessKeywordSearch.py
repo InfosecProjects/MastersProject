91e2b24d166649d4a34a4cba78d0c0a25d8b5a95
MastersProject <47504366+MastersProject@users.noreply.github.com>
2019-03-03 20:25:30 +0000

    Create ProcessKeywordSearch.py

---
+++ ProcessKeywordSearch.py
@@ -0,0 +1,15 @@
+import re
+import binascii
+file = open('/Users/Ash/Virtualbox VMs/Shared/chrome_1928.dmp')
+content = file.readlines()
+
+for line in content:
+    match = re.findall(r'latitude', line)
+
+    if match:
+      print 'found: ' + line
+      print 'hex  : ' + binascii.hexlify(line)
+      print ('\n\n')
+    else:
+      pass
+
